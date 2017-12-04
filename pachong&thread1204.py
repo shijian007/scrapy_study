a87531b642f5f733c07032709f54d443c31d9397
python-scrapy <xiezj2010@126.com>
2017-12-04 22:18:01 +0800

    爬虫BeautifulSoup
    多线程，爬虫，BeautifulSoup ，可用

--- pachong&thread1204.py
+++ pachong&thread1204.py
@@ -2,7 +2,7 @@ import re
 import requests
 from bs4 import BeautifulSoup
 import threading
-import Queue
+import queue
 import time
 
 
@@ -15,59 +15,59 @@ class Crawl():
                    "(KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36 LBBROWSER")
 
     def parse(self,url):
-        item = {}
+        items = []
         r = requests.get(url,headers = {"User-Agent":self.UA})
         html = r.content
         soup = BeautifulSoup(html,"html.parser")
-        item['title'] = soup.findAll("a", class_='name')
-        return item
+        # titles = soup.find("a", class_='name')
+        # return titles.string
+        titles = soup.findAll("a", class_='name')
+        for title in titles:
+            items.append(title.string)
+        return items
 
 
-
-class mythread():
-    def __init__(self,threadname,queue,url):
+class mythread(threading.Thread):
+    def __init__(self,threadname,queue):
+        threading.Thread.__init__(self)
         self.threadname = threadname
         self.queue = queue
-        self.url = url
 
     def run(self):
-        print("Thread ONE is running!")
+        print("Thread {0} is running!".format(self.threadname))
         threadLock.acquire()
         crawl1 = Crawl()
-        crawl1
+        item = crawl1.parse(self.queue)
+        print("77777777777777777")
+        for i in item:
+            print(self.threadname,i)
+        threadLock.release()
+        print("Thread {0} is done!".format(self.threadname))
 
 
-
-
-
-
-threadList = ["Thread-1", "Thread-2", "Thread-3"]
+threadList = ["科幻", "历史", "军事"]
 threadLock = threading.Lock()
 
 urlList = ['https://www.qidian.com/kehuan','https://www.qidian.com/lishi','https://www.qidian.com/junshi']
 threads = []
-parseQueue = Queue.Queue(6)
+parseQueue = queue.Queue(6)
 
 threadLock.acquire()
 for url in urlList:
     parseQueue.put(url)
 threadLock.release()
 
-while not parseQueue.empty():
-    pass
+# while not parseQueue.empty():
+#     pass
 
 exitflag = 1
 
 for name in threadList:
-    thread = mythread(name,parseQueue)
-    thread.start()
-    threads.append()
+    if not parseQueue.empty():
+        thread = mythread(name,parseQueue.get())
+        thread.start()
+        threads.append(thread)
 
-for thread in threads:
-    thread.join()
+for th in threads:
+    th.join()
 
-
-crawl1 = Crawl()
-item = crawl1.parse(url[0])
-
-print(item)
