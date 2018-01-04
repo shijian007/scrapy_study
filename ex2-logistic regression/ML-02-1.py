8174cb9401a54815ac17035ff17504e393ffe102
python-scrapy <xiezj2010@126.com>
2018-01-04 22:49:50 +0800

    ML-02-1

---
+++ ex2-logistic regression/ML-02-1.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+path = 'ex2data1.txt'
+data = pd.read_csv(path, header=None, names=['Exam 1', 'Exam 2', 'Admitted'])
+# data.head()
+
+positive = data[data['Admitted'].isin([1])]
+negative = data[data['Admitted'].isin([0])]
+
+fig, ax = plt.subplots(figsize=(12,8))
+ax.scatter(positive['Exam 1'], positive['Exam 2'], s=50, c='b', marker='o', label='Admitted')
+ax.scatter(negative['Exam 1'], negative['Exam 2'], s=50, c='r', marker='x', label='Not Admitted')
+ax.legend()
+ax.set_xlabel('Exam 1 Score')
+ax.set_ylabel('Exam 2 Score')
+plt.show()
+
