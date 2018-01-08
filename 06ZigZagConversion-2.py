6393974a89f992a5fd179de22158edfabba05fdd
python-scrapy <xiezj2010@126.com>
2018-01-08 18:52:49 +0800

    update

---
+++ 06ZigZagConversion-2.py
@@ -0,0 +1,55 @@
+class Solution:
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        s1 = list(s)
+        # 每组元素个数
+        n = numRows + numRows - 2
+        # 最后一组，余数
+        b = len(s1) % n
+        if b > 0:
+            for i in range(n - b):
+                s1.append('%')
+        # 组数，整数
+        a = int(len(s1) / n)
+        #每组数据
+        k = []
+        # 每排的数据
+        rowdata = []
+        if len(s) > numRows:
+            if numRows > 1:
+                for i in range(a):
+                    k.append(s1[i * n: i * n + n])
+
+        #j是每组数据长度
+        for j in range(len(k[0])):
+            if j == 0:
+                for i in range(a):
+                    rowdata.append(k[i][0])
+                # if b == 0:
+                #     rowdata.append(k[a-1][0])
+            if j >0 and j < len(k[0])-2:
+                #第i组，a是组数
+                for i in range(a):
+                    rowdata.append(k[i][j])
+                    rowdata.append(k[i][j+1])
+
+            if j == len(k[0])-1:
+                for i in range(a):
+                    rowdata.append(k[i][len(k[0])-1])
+
+
+        rowdata = ('').join(rowdata)
+        # rowdata.replace('%', '')
+        print(rowdata)
+        #PAHNAPLSIIGYIR
+
+
+if __name__=="__main__":
+    # s = 'qwertyuiopaszxcv'
+    s = 'PAYPALISHIRING'
+    P = Solution()
+    P.convert(s, 3)
